be9fa289ae2cd66ec92c8ba9f1d1fe4e72559fa6
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-04-18 09:24:22 +0200

    feat: added the placeholder component for the results

---
+++ src/components/Results/Results.tsx
@@ -0,0 +1,11 @@
+import './Results.scss'
+
+export const Results = () => {
+  return (
+    <section className='results'>
+      <img src="/images/illustration-empty.svg" alt="" />
+      <h2 className='results__title'>Results shown here</h2>
+      <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+    </section>
+  )
+}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,17 @@
 import { Form } from "../../components/Form/Form"
 import { MainHeading } from "../../components/MainHeading/MainHeading"
+import { Results } from "../../components/Results/Results"
 import './MainPage.scss'
 
 export const MainPage = () => {
   return (
     <main>
-      <MainHeading />
-      <button className="clear__form">Clear All</button>
-      <Form />
+      <div className="main__content">
+        <MainHeading />
+        <button className="clear__form">Clear All</button>
+        <Form />
+      </div>
+      <Results />
     </main>
   )
 }

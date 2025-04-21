94f436af537f931a8f46f97d31e6d8f2ce3c273d
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-04-21 09:06:48 +0200

    fix: added a fieldset with a legened element to better group the radio buttons, and changed the input types to number to better reflect what the form needs

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,30 +5,32 @@ export const Form = () => {
     <form>
       <div className="form__group">
         <label htmlFor="mortgage-amount" className='form__label'>Mortgage Amount</label>
-        <input type="text" id="mortgage-amount" className='form__field' />
+        <input type="number" id="mortgage-amount" className='form__field' />
       </div>
       <div className="form__group">
         <label htmlFor="mortgage-term" className='form__label'>Mortgage Term</label>
-        <input type="text" id="mortgage-term" className='form__field' />
+        <input type="number" id="mortgage-term" className='form__field' />
       </div>
       <div className="form__group">
         <label htmlFor="interest-rate" className='form__label'>Interest Rate</label>
-        <input type="text" id="interest-rate" className='form__field' />
+        <input type="number" id="interest-rate" className='form__field' />
       </div>
       <div className="form__group">
-        <label htmlFor="mortgage-type" className='form__label'>Mortgage Type</label>
-        <div className="form__radio">
-          <input type="radio" id="repayment" name="mortgage-type" value="repayment" className='form__radio-input' />
-          <label htmlFor="repayment" className='form__radio-label'>Repayment</label>
-        </div>
-        <div className="form__radio">
-          <input type="radio" id="interest-only" name="mortgage-type" value="interest-only" className='form__radio-input' />
-          <label htmlFor="interest-only" className='form__radio-label'>Interest Only</label>
-        </div>
+        <fieldset>
+          <legend className='form__label'>Mortgage Type</legend>
+          <div className="form__radio">
+            <input type="radio" id="repayment" name="mortgage-type" className='form__radio-input' />
+            <label htmlFor="repayment" className='form__radio-label'>Repayment</label>
+          </div>
+          <div className="form__radio">
+            <input type="radio" id="interest-only" name="mortgage-type" className='form__radio-input' />
+            <label htmlFor="interest-only" className='form__radio-label'>Interest Only</label>
+          </div>
+        </fieldset>
       </div>
       <div className="form__submit">
         <img src="/images/icon-calculator.svg" alt="" />
-        <input type="button" value="Calculate Repayments" />
+        <input type="submit" value="Calculate Repayments" />
       </div>
     </form>
   )

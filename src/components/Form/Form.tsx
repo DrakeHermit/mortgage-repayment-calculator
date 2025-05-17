32fa1c5a3a65319bf3b4fdf935d370ca300a1211
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-05-17 21:56:36 +0200

    feat: customized the radio buttons to look as in the design file
    fix: fixed the text padding of inputs to reflect the position of the labels

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -11,7 +11,7 @@ export const Form = () => {
         <label htmlFor="mortgage-amount" className='form__label'>Mortgage Amount</label>
         <div className="label__group">
           <InputLabel label={ '€' } position='left' error={Boolean(formData.errors?.mortgageAmount)} />
-          <input type="number" name="mortgageAmount" value={formData.mortgageAmount} onChange={(e) => handleInputChange(e)} id="mortgage-amount" className={`form__field ${formData.errors?.mortgageAmount && 'error'}`} />
+          <input type="number" name="mortgageAmount" value={formData.mortgageAmount} onChange={(e) => handleInputChange(e)} id="mortgage-amount" className={`form__field p-left-4 ${formData.errors?.mortgageAmount && 'error'}`} />
         </div>
           {formData.errors?.mortgageAmount && <p className='error__message'>{formData.errors.mortgageAmount}</p>}
       </div>
@@ -19,7 +19,7 @@ export const Form = () => {
         <label htmlFor="mortgage-term" className='form__label'>Mortgage Term</label>
         <div className="label__group">
           <InputLabel label={ 'years' } position='right' error={Boolean(formData.errors?.mortgageTerm)} />
-          <input type="number" name="mortgageTerm" value={formData.mortgageTerm} onChange={(e) => handleInputChange(e)} id="mortgage-term" className={`form__field ${formData.errors?.mortgageTerm && 'error'}`} />
+          <input type="number" name="mortgageTerm" value={formData.mortgageTerm} onChange={(e) => handleInputChange(e)} id="mortgage-term" className={`form__field p-right-3 ${formData.errors?.mortgageTerm && 'error'}`} />
         </div>
         {formData.errors?.mortgageTerm && <p className='error__message'>{ formData.errors.mortgageTerm }</p>}
       </div>
@@ -27,18 +27,18 @@ export const Form = () => {
         <label htmlFor="interest-rate" className='form__label'>Interest Rate</label>
         <div className="label__group">
           <InputLabel label={ '%' } position='right' error={Boolean(formData.errors?.interestRate)} />
-          <input type="number" name="interestRate" value={formData.interestRate} onChange={(e) => handleInputChange(e)} id="interest-rate" className={`form__field ${formData.errors?.interestRate && 'error'}`} />
+          <input type="number" name="interestRate" value={formData.interestRate} onChange={(e) => handleInputChange(e)} id="interest-rate" className={`form__field p-right-3 ${formData.errors?.interestRate && 'error'}`} />
         </div>
         {formData.errors?.interestRate && <p className='error__message'>{ formData.errors.interestRate }</p>}
       </div>
-      <div className="form__group">
+      <div className={"form__group"}>
         <fieldset>
           <legend className='form__label mb-1'>Mortgage Type</legend>
-          <div className="form__radio">
+          <div className={`form__radio ${formData.mortgageType === 'repayment' ? 'checked' : ''}`}>
             <input type="radio" id="repayment" checked={formData.mortgageType === 'repayment'} value='repayment' onChange={(e) => handleInputChange(e)} name="mortgageType" className='form__radio-input' />
             <label htmlFor="repayment" className='form__radio-label'>Repayment</label>
           </div>
-          <div className="form__radio">
+          <div className={`form__radio ${formData.mortgageType === 'interest-only' ? 'checked' : ''}`}>
             <input type="radio" id="interest-only" checked={formData.mortgageType === "interest-only"} value="interest-only" onChange={(e) => handleInputChange(e)} name="mortgageType" className='form__radio-input' />
             <label htmlFor="interest-only" className='form__radio-label'>Interest Only</label>
           </div>
